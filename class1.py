0560209f61afc77ba56d709c851d6871a908988b
PushpithaJR26 <167069890+PushpithaJR26@users.noreply.github.com>
2024-04-18 11:30:14 +0530

    class1.py

---
+++ class1.py
@@ -0,0 +1,9 @@
+class Box:
+    def __init__(self,nameOfStduent,rollNo):
+        self.name = nameOfStduent
+        self.rollNo = rollNo
+
+obj1 = Box("eva",34)
+obj2 = Box("rora",23)
+
+print(obj1.name)
