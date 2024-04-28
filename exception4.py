76f4516cc0d2bcade198a5a59c5fb29b752a485d
PushpithaJR26 <167069890+PushpithaJR26@users.noreply.github.com>
2024-04-28 10:19:08 +0530

    Create exception4.py

---
+++ exception4.py
@@ -0,0 +1,9 @@
+print("starting line")
+try:
+    a = 10/5
+except:
+    print("some exception raised")
+else:
+    print("no exception raised,everything workes perfect")
+print("outside the block")
+
